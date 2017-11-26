956bf3bdcf93e795ffe58c5031a7509d535328d2
Zaexides <JeroenEndenburg@hotmail.com>
2017-11-26 15:42:13 +0100

    Removed "[CLOSED BETA]" part in mod version

--- main/java/zaexides/steamworld/ModInfo.java
+++ main/java/zaexides/steamworld/ModInfo.java
@@ -4,7 +4,7 @@ public class ModInfo
 {
 	public static final String MODID = "steamworld";
     public static final String MODNAME = "SteamWorld";
-    public static final String VERSION = "[CLOSED BETA] 1.12.2-0.2.3.2";
+    public static final String VERSION = "1.12.2-0.2.3.2";
     
     public static final String CLIENT_PROXY = "zaexides.steamworld.proxy.ClientProxy";
     public static final String COMMON_PROXY = "zaexides.steamworld.proxy.CommonProxy";
